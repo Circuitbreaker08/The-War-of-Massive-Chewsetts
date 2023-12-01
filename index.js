ee4552b6d83c3389682baada96b5038cea4f5ca0
Circuitbreaker08 <53588678+Circuitbreaker08@users.noreply.github.com>
2023-12-01 18:59:24 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const c = document.getElementById("c")
+const ctx = c.getContext("2d")
+
+c.height = 32 * 36
+c.width = 32 * 12
+
+class BoardObject {
+	constructor() {
+  
+  }
+  tick() {
+  
+  }
+  new_turn() {
+  
+  }
+}
+
